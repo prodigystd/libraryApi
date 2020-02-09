1137736a03579de9ab9dad433a7b84f29b35dd05
s.navruzov <navruzov@infosystems.uz>
2020-02-09 17:30:04 +0500

    AddVendorName in namespace

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
-
-
-spl_autoload_register(function ($className) {
-    $className = str_replace("\\", DIRECTORY_SEPARATOR, $className);
-    include_once $_SERVER['DOCUMENT_ROOT'] . '/' . $className . '.php';
-});
-
+function autoload($className)
+{
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    $className = substr($className, strpos($className, '\\') + 1); //cutting VendorName
+    if ($lastNsPos = strrpos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+    require $fileName;
+}
+spl_autoload_register('autoload');
 
 $url = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
@@ -20,7 +28,7 @@ $routeKeyToFind = $method . ', ' . rtrim($url, "/");
 
 if (isset($routes[$routeKeyToFind])) {
     $action = explode('@', $routes[$routeKeyToFind]);
-    call_user_func(["\Controllers\\". $action[0], $action[1]]);
+    call_user_func(["\LibraryApi\Controllers\\". $action[0], $action[1]]);
 } else {
     echo 'Route is not found';
 }
