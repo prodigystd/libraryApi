11d46b818437803414f062351475dcc14f13b1f9
prodigystd <prodigystd@users.noreply.github.com>
2022-05-17 11:19:29 +0500

    Update routes.php
    fix: update controller method name

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 
 
 $routes = [
-    'GET, /book/books-by-author' => 'BookController@ByAuthor',
+    'GET, /book/books-by-author' => 'BookController@byAuthor',
     'GET, /book/books-by-author-count' => 'BookController@byAuthorCount',
     'GET, /author/authors-by-book' => 'AuthorController@byBook',
 ];
