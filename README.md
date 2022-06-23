3a431debaf4bcfc16a3440d11c4a6b0d2e03f80b
Sergey Navruzov <prodigystd@gmail.com>
2022-06-23 14:53:05 +0500

    Add english readme and database data

--- README.md
+++ README.md
@@ -1,5 +1,45 @@
 # libraryApi
 
+Design a database schema and a backend for storing a library. It should store authors and their books. User interaction occurs through HTTP requests to the API. All responses are JSON objects.
+
+The server implements the following methods:
+
+selecting all books by one author
+getting the author on the book
+getting a list of books written by exactly 2 co-authors. Result: book - number of co-authors.
+Requirements:
+
+code according to PSR standards and
+it is forbidden to use different frameworks
+
+
+# Usage:
+In the root folder run:
+docker-compose-up
+
+# API description:
+
+listing all books by the same author:<br />
+Request type: GET <br />
+Parameter: author_name<br />
+Parameter type: string<br />
+http://localhost:8080/book/books-by-author?author_name=Antoine
+
+getting the author by the book:<br />
+Request type: GET <br />
+Parameter: book_name<br />
+Parameter type: string<br />
+http://localhost:8080/author/authors-by-book?book_name=Harry<br />
+
+
+getting a list of books written by exactly 2 co-authors. Result: book - number of coauthors:<br />
+Request type: GET <br />
+Parameter: author_count<br />
+Parameter type: integer<br />
+http://localhost:8080/book/books-by-author-count?author_count=2<br />
+
+#
+
 Спроектировать схему БД и backend для хранения библиотеки. Интересуют авторы и книги. Взаимодействие с пользователем происходит посредством HTTP запросов к API. Все ответы представляют собой JSON объекты.
 
 Сервер реализует следующие методы:
@@ -9,7 +49,7 @@
 выдача списка книг, написанных ровно 3 соавторами. Результат: книга - количество соавторов.
 Требования:
 
-оформить код по стандартам PSR
+оформить код по стандартам PSR и
 запрещается использовать различные framework’и
 
 
@@ -23,18 +63,18 @@ docker-compose up
 Тип запроса: GET <br />
 Параметр: author_name<br />
 Тип параметра: строка<br />
-http://localhost:8080/book/books-by-author?author_name=Толстой
+http://localhost:8080/book/books-by-author?author_name=Antoine
 
 выдача автора по книге:<br />
 Тип запроса: GET <br />
 Параметр: book_name<br />
 Тип параметра: строка<br />
-http://localhost:8080/author/authors-by-book?book_name=Война<br />
+http://localhost:8080/author/authors-by-book?book_name=Harry<br />
 
 
-выдача списка книг, написанных ровно 3 соавторами. Результат: книга - количество соавторов:<br />
+выдача списка книг, написанных ровно 2 соавторами. Результат: книга - количество соавторов:<br />
 Тип запроса: GET <br />
 Параметр: author_count<br />
 Тип параметра: целое число<br />
-http://localhost:8080/book/books-by-author-count?author_count=3<br />
+http://localhost:8080/book/books-by-author-count?author_count=2<br />
 
---
+++ dump.sql
@@ -0,0 +1,115 @@
+-- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: library
+-- ------------------------------------------------------
+-- Server version	5.7.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `library`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `library` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `library`;
+
+--
+-- Table structure for table `author`
+--
+
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author`
+--
+
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES (1,'Charles Dickens','1812-02-07','An English writer and social critic. He created some of the world\'s best-known fictional characters and is regarded by many as the greatest novelist of the Victorian era.[1] His works enjoyed unprecedented popularity during his lifetime and, by the 20th century, critics and scholars had recognised him as a literary genius. His novels and short stories are widely read today.'),(2,'Antoine de Saint-Exupéry','1990-06-29','A French writer, poet, aristocrat, journalist and pioneering aviator. He became a laureate of several of France\'s highest literary awards and also won the United States National Book Award.[6] He is best remembered for his novella The Little Prince (Le Petit Prince) and for his lyrical aviation writings, including Wind, Sand and Stars and Night Flight.'),(3,'Terry Pratchett','1948-04-28','An English humourist, satirist, and author of fantasy novels, especially comical works.[1] He is best known for his Discworld series of 41 novels.'),(4,'Joanne Rowling','1965-07-31','A British author and philanthropist. She wrote a seven-volume children\'s fantasy series, Harry Potter, published from 1997 to 2007. The series has been enormously successful: it has sold over 500 million copies, been translated into at least 70 languages, and spawned a global media franchise including films and video games. The Casual Vacancy (2012) was her first novel for adults. She writes Cormoran Strike, an ongoing crime fiction series, as Robert Galbraith.'),(5,'Neil Gaiman','1960-11-10','An English author of short fiction, novels, comic books, graphic novels, nonfiction, audio theatre, and films. His works include the comic book series The Sandman and novels Stardust, American Gods, Coraline, and The Graveyard Book. He has won numerous awards, including the Hugo, Nebula, and Bram Stoker awards, as well as the Newbery and Carnegie medals. He is the first author to win both the Newbery and the Carnegie medals for the same work, The Graveyard Book (2008).');
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `author_book`
+--
+
+DROP TABLE IF EXISTS `author_book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author_book` (
+  `author_id` int(11) DEFAULT NULL,
+  `book_id` int(11) DEFAULT NULL,
+  KEY `author_book_fk_author_id` (`author_id`),
+  KEY `author_book_fk_book_id` (`book_id`),
+  CONSTRAINT `author_book_fk_author_id` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`),
+  CONSTRAINT `author_book_fk_book_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author_book`
+--
+
+LOCK TABLES `author_book` WRITE;
+/*!40000 ALTER TABLE `author_book` DISABLE KEYS */;
+INSERT INTO `author_book` VALUES (1,1),(2,2),(3,4),(5,4),(4,3);
+/*!40000 ALTER TABLE `author_book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
+  `year` int(11) NOT NULL,
+  `genre` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (1,'A Tale of Two Cities','A Tale of Two Cities is an 1859 historical novel by Charles Dickens, set in London and Paris before and during the French Revolution. The novel tells the story of the French Doctor Manette, his 18-year-long imprisonment in the Bastille in Paris, and his release to live in London with his daughter Lucie whom he had never met. The story is set against the conditions that led up to the French Revolution and the Reign of Terror. ',1859,'Historical fiction'),(2,'The Little Prince','The Little Prince (French: Le Petit Prince, pronounced [lə p(ə)ti pʁɛ̃s]) is a novella by French aristocrat, writer, and military aviator Antoine de Saint-Exupéry. It was first published in English and French in the United States by Reynal & Hitchcock in April 1943 and was published posthumously in France following liberation; Saint-Exupéry\'s works had been banned by the Vichy Regime. The story follows a young prince who visits various planets in space, including Earth, and addresses themes of loneliness, friendship, love, and loss. Despite its style as a children\'s book, The Little Prince makes observations about life, adults and human nature.',1943,'Novella'),(3,'Harry Potter and the Philosopher\'s Stone','Harry Potter and the Philosopher\'s Stone is a fantasy novel written by British author J. K. Rowling. The first novel in the Harry Potter series and Rowling\'s debut novel, it follows Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday, when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry. Harry makes close friends and a few enemies during his first year at the school and with the help of his friends, he faces an attempted comeback by the dark wizard Lord Voldemort, who killed Harry\'s parents, but failed to kill Harry when he was just 15 months old.',1997,'Fantasy'),(4,'Good Omens','The book is a comedy about the birth of the son of Satan and the coming of the End Times. There are attempts by the angel Aziraphale and the demon Crowley to sabotage the coming of the end times, having grown accustomed to their comfortable surroundings in England.',1990,'Fantasy');
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-23  9:35:42
