c509aba7aa35ba946d5dd228a1ea311ec1748c2c
Sergey <prodigystd@gmail.com>
2022-07-16 16:43:33 +0500

    refactor: move autoload to the file

--- index.php
+++ index.php
@@ -2,21 +2,7 @@
 
 use LibraryApi\Microkernel\Microkernel;
 
-function autoload($className)
-{
-    $className = ltrim($className, '\\');
-    $fileName  = '';
-    $namespace = '';
-    $className = substr($className, strpos($className, '\\') + 1); //cutting VendorName
-    if ($lastNsPos = strrpos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-    require_once $fileName;
-}
-spl_autoload_register('autoload');
+require __DIR__ . '/autoloader.php';
 
 $microkernel = new Microkernel();
 $microkernel->boot();
