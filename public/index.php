d40a021ee885c5b8e525390be7210594b2e2a5b2
Sergey <prodigystd@gmail.com>
2022-09-25 13:31:41 +0500

    refactor: move index file to public

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use LibraryApi\Microkernel\Container\Container;
 use LibraryApi\Microkernel\Microkernel;
 use LibraryApi\Microkernel\MicrokernelInterface;
 
-require __DIR__ . '/autoloader.php';
+require __DIR__ . '/../autoloader.php';
 
 $container = Container::instance();
 $container->bind(MicrokernelInterface::class, new Microkernel());
